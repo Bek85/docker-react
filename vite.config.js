f38dfa4116fd8b848ac5c5d27da62f4ff38ccc59
Bekzod <stx.webdev@gmail.com>
2025-07-19 15:18:03 +0500

    feat(deploy): add GitHub Actions workflow for deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// If deploying to https://yourname.github.io/repo-name
+const base = 'https://github.com/Bek85/docker-react'
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: base,
   server: {
     host: '0.0.0.0',
     port: 5173
